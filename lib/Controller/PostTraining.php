58ffff11cbfd2100cdf2ae2fa4e16e0927349d2f
Taichi Shiozawa <61722629+feen-11@users.noreply.github.com>
2020-11-06 17:39:22 +0900

    Update PostTraining.php

--- lib/Controller/PostTraining.php
+++ lib/Controller/PostTraining.php
@@ -25,7 +25,7 @@ class PostTraining extends Controller {
             $this->postProcess();
     }
   }
-
+// 投稿処理
     public function postProcess(){
 
       try{
@@ -52,7 +52,7 @@ class PostTraining extends Controller {
         exit;
     }
     
-
+// POST中身チェック
     public function validate(){
       if(!isset($_POST['token']) || $_POST['token'] !== $_SESSION['token']){
         throw new InvalidToken();

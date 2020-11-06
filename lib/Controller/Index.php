f1f29cb662c7dc8827b38602eff733b7abab1515
Taichi Shiozawa <61722629+feen-11@users.noreply.github.com>
2020-11-06 17:33:50 +0900

    Update Index.php

--- lib/Controller/Index.php
+++ lib/Controller/Index.php
@@ -14,50 +14,50 @@ class Index extends Controller {
   
   public function run() {
     if (!$this->loginCheck()) {
-      // login
+      // redirect to login
       header('Location: ' . SITE_URL . '/login.php');
       exit;
     }
     if (!$this->setUpCheck()) {
-      // setup
+      //redirect to setup
       header('Location: ' . SITE_URL . '/setUp.php');
       exit;
     }
     if (!$this->startCheck()) {
-      // start
+      //redirect to start
       header('Location: ' . SITE_URL . '/start.php');
       exit;
     }
   }
-
+// 日記読み込み
   public function readPosts(){
       $app = new Post();
       $posts = $app->readPost();
       return $posts;
   }
-
+// 食事読み込み
   public function readFoods(){
     $app = new Post();
       $posts = $app->readFood();
       return $posts;
   }
-
+// トレーニング読み込み
   public function readTrainings(){
     $app = new Post();
       $posts = $app->readTraining();
       return $posts;
   }
-
+// ログイン日時読み込み
   public function readLoginTimes(){
     $app = new Model();
     return $app->getLoginDate();
   }
-
+// 合計摂取カロリー計算
   public function totalIntakeCalorie(){
     $app = new Post();
     return $app->getTotalIntakeCalorie();
   }
-
+// 合計消費カロリー計算
   public function totalBurnCalorie(){
     $app = new Post();
     return $app->getTotalBurnCalorie();
